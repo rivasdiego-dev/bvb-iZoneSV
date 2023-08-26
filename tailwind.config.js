103cdabb577b304772b0f7da4a72c333070c0252
rivasdiego-dev <rivasdiego.dev@gmail.com>
2023-08-26 15:28:20 -0600

    Config project colors :lipstick:

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,34 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        'primary': {
+          '50': '#fffeea',
+          '100': '#fff9c5',
+          '200': '#fff485',
+          '300': '#ffe846',
+          '400': '#ffd81b',
+          default: '#ffba08',
+          '600': '#e28d00',
+          '700': '#bb6302',
+          '800': '#984c08',
+          '900': '#7c3e0b',
+          '950': '#482000',
+        },
+        'secondary': {
+          '50': '#f2f9fd',
+          '100': '#e4f0fa',
+          '200': '#c4e1f3',
+          '300': '#8fc9ea',
+          '400': '#54addc',
+          '500': '#2e93c9',
+          default: '#1d70a2',
+          '700': '#1a5e8a',
+          '800': '#195073',
+          '900': '#1a4360',
+          '950': '#112b40',
+        },
+      }
     },
   },
   plugins: [],
