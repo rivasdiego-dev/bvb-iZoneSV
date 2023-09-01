bc42251e4a2e89b6f15374451a7ad57d0109d58f
rivasdiego-dev <rivasdiego.dev@gmail.com>
2023-09-01 12:12:00 -0600

    Add 'loading' state to hook :sparkles:

--- hooks/useFetchEvents.ts
+++ hooks/useFetchEvents.ts
@@ -3,22 +3,26 @@ import { GetAllEvents } from "@/firebase/services/events";
 import { useEffect, useState } from "react";
 
 export default function useFetchEvents() {
-    const [volleyEvents, setVolleyEvents] = useState<VolleyEvent[]>([]);
-  
-    async function fetchEvents() {
-      try {
-        const fetchedEvents = await GetAllEvents();
-        if (fetchedEvents) {
-          setVolleyEvents([...fetchedEvents]);
-        }
-      } catch (error) {
-        // Handle error if needed
+  const [volleyEvents, setVolleyEvents] = useState<VolleyEvent[]>([]);
+  const [loading, setLoading] = useState(true); // Add loading state
+
+  async function fetchEvents() {
+    try {
+      const fetchedEvents = await GetAllEvents();
+      if (fetchedEvents) {
+        setVolleyEvents([...fetchedEvents]);
       }
+    } catch (error) {
+      // Handle error if needed
+    } finally {
+      setLoading(false); // Set loading to false when fetch completes
     }
-  
-    useEffect(() => {
-      fetchEvents();
-    }, []);
-  
-    return volleyEvents;
   }
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
+  // Return both the data and loading state
+  return { volleyEvents, loading };
+}
